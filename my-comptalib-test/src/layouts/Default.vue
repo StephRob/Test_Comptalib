b24a667481d552168377a4f6a8b6da79b39d0dfa
Stéphane Robert <rob.steph@free.fr>
2021-09-08 09:29:28 +0200

    created header layout

--- my-comptalib-test/src/layouts/Default.vue
+++ my-comptalib-test/src/layouts/Default.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
+  <div>
+    <Header />
     <slot/>
   </div>
 </template>
@@ -20,6 +12,16 @@ query {
   }
 }
 </static-query>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style>
 body {
---
+++ my-comptalib-test/src/layouts/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <b-navbar class="container">
+      <template #brand>
+        <b-navbar-item fixed-top tag="router-link" :to="{ path: '/' }">
+          <img src="../assets/logo_comptalib.svg" alt="logo Comptalib" />
+          <span class="logo-text blue">Lunch</span>
+        </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <g-link to="/Restaurant">
+              <b-button type="is-info is-light">
+                <strong>Voir le restaurant</strong>
+              </b-button>
+            </g-link>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.logo-text {
+  font-size: 27px;
+  font-style: italic;
+}
+
+.blue,
+.background-blue {
+  color: #1854f2;
+}
+</style>
